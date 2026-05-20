854751e734653c89f1d42cce8b5405bc04816100
Haruko228 <ivan.tsipak@gmail.com>
2026-05-20 13:16:50 +0300

    Update practice11.asm

--- dev-env/practice11.asm
+++ dev-env/practice11.asm
@@ -3,14 +3,14 @@ section .data
 
 section .bss
     buffer      resb 16
-    line_buf    resb 128    ; Буфер для формування рядка
+    line_buf    resb 128   
     h_val       resd 1
 
 section .text
     global _start
 
 _start:
-    ; --- I/O: Читання висоти h ---
+
     mov eax, 3          ; sys_read
     mov ebx, 0          ; stdin
     mov ecx, buffer
@@ -19,15 +19,14 @@ _start:
     call atoi
     mov [h_val], eax
 
-    ; --- loops: Вкладені цикли для ялинки ---
-    xor esi, esi        ; esi = i (поточний рядок від 0 до h-1)
+  
+    xor esi, esi       
 .main_loop:
     cmp esi, [h_val]
     je .exit
 
-    mov edi, line_buf   ; edi вказує на початок буфера рядка
+    mov edi, line_buf  
 
-    ; 1. Цикл пробілів: кількість = h - i - 1
     mov ecx, [h_val]
     sub ecx, esi
     dec ecx
@@ -39,7 +38,6 @@ _start:
     loop .spaces_lp
 
 .stars_prep:
-    ; 2. Цикл зірочок: кількість = 2 * i + 1
     mov ecx, esi
     shl ecx, 1          ; ecx = 2 * i
     inc ecx             ; ecx = 2 * i + 1
@@ -48,26 +46,22 @@ _start:
     inc edi
     loop .stars_lp
 
-    ; 3. Додаємо символ нового рядка в кінець буфера
     mov byte [edi], 0xA
     inc edi
 
-    ; --- logic: Один вивід на рядок через підпрограму ---
     mov eax, edi
-    sub eax, line_buf   ; eax = довжина сформованого рядка (len)
-    mov ecx, line_buf   ; ecx = адреса буфера (buf)
+    sub eax, line_buf 
+    mov ecx, line_buf   
     call print_line
 
     inc esi
     jmp .main_loop
 
 .exit:
-    ; --- memory: Вихід з програми ---
     mov eax, 1          ; sys_exit
     xor ebx, ebx
     int 0x80
 
-; --- Підпрограма друку рядка print_line(buf, len) ---
 print_line:
     pusha
     mov edx, eax        ; edx = len
@@ -77,7 +71,6 @@ print_line:
     popa
     ret
 
-; --- Допоміжна підпрограма atoi ---
 atoi:
     xor eax, eax
     mov edi, ecx
